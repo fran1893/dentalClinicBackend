f76885f754516a73656f6324916449c52606163f
Francisco Diaz <diazsalerno2@outlook.es>
2023-04-14 18:12:05 +0200

    Create usuarios.js model and migration

--- models/roles.js
+++ models/roles.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Roles extends Model {
     /**
@@ -13,11 +11,15 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Roles.init({
-    nombre_rol: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Roles',
-  });
+  Roles.init(
+    {
+      nombre_rol: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Roles",
+      tableName: "roles"
+    }
+  );
   return Roles;
 };
---
+++ models/usuarios.js
@@ -0,0 +1,71 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Usuarios extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      /* Usuarios y Roles (1:N) */
+      Usuarios.belongsTo(models.Roles, {
+        as: "roles",
+        foreignKey: "id_roles", // foreignKey de Usuarios
+      });
+    }
+  }
+  Usuarios.init(
+    {
+      nombre: {
+        type: DataTypes.STRING,
+        validate: {
+          is: /^[a-zA-Z]+(([',.-][a-zA-Z ])?[a-zA-Z]*)*$/,
+        },
+      },
+      apellidos: {
+        type: DataTypes.STRING,
+        validate: {
+          is: /^[a-zA-Z]+(([',.-][a-zA-Z ])?[a-zA-Z]*)*$/,
+        },
+      },
+      edad: {
+        type: DataTypes.INTEGER,
+        validate: {
+          isInt: {
+            msg: "Debe ser un número entero",
+          },
+          min: {
+            msg: "La edad debe ser mayor que 1",
+            args: 1,
+          },
+          max: 100,
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: { isEmail: true, isLowercase: true },
+      },
+      telefono: {
+        type: DataTypes.INTEGER,
+        validate: {
+          isNumeric: true,
+          isInt: {
+            msg: "El número telefónico tiene que ser entero",
+          },
+          len: [9],
+        },
+      },
+      password: { type: DataTypes.STRING },
+      id_roles: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Usuarios",
+      tableName: "usuarios",
+    }
+  );
+  return Usuarios;
+};
