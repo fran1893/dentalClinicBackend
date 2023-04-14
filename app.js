09761f984392a83e4b5e168d0b23a6f499930068
Francisco Diaz <diazsalerno2@outlook.es>
2023-04-14 15:22:15 +0200

    add dotenv to app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const router = require("./router");
+require("dotenv").config();
 
 const app = express();
 
