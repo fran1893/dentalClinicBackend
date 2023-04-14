3578d10f57537168b4eb15a8e633963d0ba9a613
Francisco Diaz <diazsalerno2@outlook.es>
2023-04-14 18:36:30 +0200

    Add association Roles with Usuarios

--- models/roles.js
+++ models/roles.js
@@ -8,7 +8,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Roles.hasMany(models.Usuarios, {
+        foreignKey: "id_roles", // foreignKey en modelo Usuario
+      });
     }
   }
   Roles.init(
