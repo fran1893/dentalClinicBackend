bb52c8e0d165a07139e2e1eed7de68da1c3da60c
Francisco Diaz <diazsalerno2@outlook.es>
2023-04-14 15:19:27 +0200

    create basic router

---
+++ router.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const indexRouter = require("./routes/index");
+// const alumnosRouter = require("./routes/alumnos");
+//const nacionalidadesRouter = require("./routes/nacionalidades");
+
+/* home page */
+router.use("/", indexRouter);
+
+
+
+module.exports = router;
