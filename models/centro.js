a513cda5223266390497ed7e2c5224c2c6eb02bb
Francisco Diaz <diazsalerno2@outlook.es>
2023-04-14 19:12:38 +0200

    Add centro.js model, migration and association with cita table

--- models/centro.js
+++ models/centro.js
@@ -1,24 +1,28 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class centro extends Model {
+  class Centro extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Centro.hasMany(models.Cita, {
+        foreignKey: "id_centro", // foreignKey en modelo Cita
+      });
     }
   }
-  centro.init({
-    nombre_lugar: DataTypes.STRING,
-    direccion: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'centro',
-  });
-  return centro;
+  Centro.init(
+    {
+      nombre_lugar: DataTypes.STRING,
+      direccion: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Centro",
+      tableName: "centro",
+    }
+  );
+  return Centro;
 };
