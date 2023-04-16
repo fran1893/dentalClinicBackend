c4836ffa4fc4258eb0cf328cf50732179cbcd74e
Francisco Diaz <diazsalerno2@outlook.es>
2023-04-16 15:27:10 +0200

    create seed for pacientes

--- seeders/05-seed-paciente.js
+++ seeders/05-seed-paciente.js
@@ -1,25 +1,19 @@
 "use strict";
 const { Op } = require("sequelize");
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-     */
+    await queryInterface.bulkInsert(
+      "doctores",
+      [{ id_usuario: 1, createdAt: new Date(), updatedAt: new Date() }],
+      {}
+    );
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete("doctores", {
+      [Op.or]: [{ id_usuario: 1 }],
+    });
   },
 };
