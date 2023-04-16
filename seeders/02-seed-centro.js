615c0ccdf174430ce53de3ba0364f9a265c805a3
Francisco Diaz <diazsalerno2@outlook.es>
2023-04-16 14:03:16 +0200

    Create seed for centros table

--- seeders/02-seed-centro.js
+++ seeders/02-seed-centro.js
@@ -1,25 +1,43 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("centros", [
+      {
+        nombre_lugar: "Clinica Barcelona",
+        direccion: "Gran via 576",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        nombre_lugar: "Clinica Madrid",
+        direccion: "C. de Juan de Mariana 15",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        nombre_lugar: "Clinica Valencia",
+        direccion: "Av. de Catalunya 11",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("centros", {
+      [Op.or]: [
+        {
+          nombre_lugar: "Clinica Barcelona",
+        },
+        {
+          nombre_lugar: "Clinica Madrid",
+        },
+        {
+          nombre_lugar: "Clinica Valencia",
+        },
+      ],
+    });
+  },
 };
