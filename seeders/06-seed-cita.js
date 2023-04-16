b3811ea6b47c6e510e90d4ffa7a5e53d099e2a1f
Francisco Diaz <diazsalerno2@outlook.es>
2023-04-16 15:39:30 +0200

    create seed for citas table

--- seeders/06-seed-cita.js
+++ seeders/06-seed-cita.js
@@ -1,26 +1,30 @@
-'use strict';
+"use strict";
 const { Op } = require("sequelize");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "citas",
+      [
+        {
+          id_doctor: 3,
+          id_paciente: 1,
+          id_centro: 1,
+          fecha: new Date(),
+          horario: "15:45:00.0000000",
+          tratamiento: "extraer muela del juicio",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("citas", {
+      [Op.or]: [{ id_doctor: 3 }],
+    });
+  },
 };
