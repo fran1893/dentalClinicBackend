a73d340627a784df9146a5009f2d0c3807aa239f
Francisco Diaz <diazsalerno2@outlook.es>
2023-04-16 13:17:23 +0200

    Add association with centro.js and cita.js

--- models/centro.js
+++ models/centro.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Centro.hasMany(models.Cita, {
+      Centro.belongsTo(models.Cita, {
         foreignKey: "id_centro", // foreignKey en modelo Cita
       });
     }
