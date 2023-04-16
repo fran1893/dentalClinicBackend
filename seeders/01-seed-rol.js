9954e383a84917df2c7289b320247d6fcd5b4400
Francisco Diaz <diazsalerno2@outlook.es>
2023-04-16 15:06:52 +0200

    corrected data on rol and centro seeds

--- seeders/01-seed-rol.js
+++ seeders/01-seed-rol.js
@@ -4,18 +4,20 @@ const { Op } = require("sequelize");
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert("roles", [
-      { nombre_rol: "paciente", createdAt: new Date(), updatedAt: new Date() },
-      { nombre_rol: "doctor", createdAt: new Date(), updatedAt: new Date() },
-      { nombre_rol: "admin", createdAt: new Date(), updatedAt: new Date() },
-    ]);
+    await queryInterface.bulkInsert(
+      "roles",
+      [
+        { nombre_rol: "user", createdAt: new Date(), updatedAt: new Date() },
+        { nombre_rol: "admin", createdAt: new Date(), updatedAt: new Date() },
+      ],
+      {}
+    );
   },
 
   async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete("roles", {
       [Op.or]: [
-        { nombre_rol: "paciente" },
-        { nombre_rol: "doctor" },
+        { nombre_rol: "user" },
         { nombre_rol: "admin" },
       ],
     });
