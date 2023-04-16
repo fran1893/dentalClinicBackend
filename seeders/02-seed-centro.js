37c9327e24ee06bdc135b656d7994e6546a54923
Francisco Diaz <diazsalerno2@outlook.es>
2023-04-16 15:18:08 +0200

    create seed for usuario and require Op on centro seed

--- seeders/02-seed-centro.js
+++ seeders/02-seed-centro.js
@@ -1,4 +1,5 @@
 "use strict";
+const { Op } = require("sequelize");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
--- seeders/03-seed-usuario.js
+++ seeders/03-seed-usuario.js
@@ -1,25 +1,64 @@
 'use strict';
+const { Op } = require("sequelize");
+const bcrypt = require('bcrypt');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert(
+      "centros",
+      [
+        {
+          nombre: "Luis",
+          apellidos: "Suarez",
+          edad: 19,
+          email: "bolso@gmail.com",
+          telefono: 616540798,
+          password: bcrypt.hashSync('bolso', 10),
+          id_rol: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          nombre: "Francisco",
+          apellidos: "Diaz",
+          edad: 29,
+          email: "pancho@gmail.com",
+          telefono: 618560718,
+          password: bcrypt.hashSync('admin', 10),
+          id_rol: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          nombre: "Alberto",
+          apellidos: "Mendez",
+          edad: 45,
+          email: "alberto@gmail.com",
+          telefono: 616744498,
+          password: bcrypt.hashSync('alberto', 10),
+          id_rol: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete("centros", {
+      [Op.or]: [
+        {
+          nombre: "Luis",
+        },
+        {
+          nombre: "Francisco",
+        },
+        {
+          nombre: "Alberto",
+        },
+      ],
+    });
   }
 };
