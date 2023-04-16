76b42c05953e2bf9f1045064624f93bc2ffea685
Francisco Diaz <diazsalerno2@outlook.es>
2023-04-16 15:23:20 +0200

    corrected table name to "citas"

--- migrations/06-create-cita.js
+++ migrations/06-create-cita.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Citas", {
+    await queryInterface.createTable("citas", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -50,6 +50,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Citas");
+    await queryInterface.dropTable("citas");
   },
 };
